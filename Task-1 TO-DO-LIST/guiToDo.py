bb214cc3e72d438e5afe0c504d51e556bb46e4a6
Sourabh Bundele <sourabhbundele@gmail.com>
2023-11-07 18:25:42 +0530

    Refactor: updated some styling

--- Task-1 TO-DO-LIST/guiToDo.py
+++ Task-1 TO-DO-LIST/guiToDo.py
@@ -5,21 +5,21 @@ root = Tk()
 # root.title is used to add the title of the app
 root.title("To-Do-List")
 # root.geometry is used to give the width and height of the workspace/app
-root.geometry("500x500")
+root.geometry("600x600")
 
 # Define fonts
-my_font = Font(family="Times New Roman", size=30,weight="bold")
+my_font = Font(family="Times New Roman", size=30, weight="bold")
 
-# to create afrom we frame method
+# to create a frame we used Frame method
 my_frame = Frame(root)
 my_frame.pack(pady=10)
 
 # Created a Listbox
 my_list = Listbox(my_frame,
                   font=my_font,
-                  width=25,
-                  height=5,
-                  bg="#91A3B0",
+                  width=29,
+                  height=8,
+                  bg="#435585",
                   fg="white",
                   bd=0,
                   highlightthickness=0,
@@ -35,15 +35,15 @@ for item in stuff:
 my_scroll = Scrollbar(my_frame)
 my_scroll.pack(side=RIGHT, fill=BOTH)
 
-# Add Scrollbar
+# Added Scrollbar
 my_list.config(yscrollcommand=my_scroll.set)
 my_scroll.config(command=my_list.yview)
 
-# create a entry box to add items to the list
-my_entry = Entry(root, font=("Helvetica", 24))
+# created entry box to add items to the list
+my_entry = Entry(root, width="26", font=("Helvetica", 15))
 my_entry.pack(pady=20)
 
-# Create a button frame
+# Created a button frame
 button_frame = Frame(root)
 button_frame.pack(pady=20)
 
@@ -57,12 +57,12 @@ def add_item():
     my_entry.delete(0, END)
 
 
-# Add Some buttons
-delete_button = Button(button_frame, text="Delete Item", command = delete_item)
-add_button = Button(button_frame, text="Add Item", command = add_item)
+# Added Some buttons
+delete_button = Button(button_frame, text="Delete Item", command=delete_item)
+add_button = Button(button_frame, text="Add Item", command=add_item)
 
 
-delete_button.grid(row=0,column=0)
+delete_button.grid(row=0, column=0)
 add_button.grid(row=0, column=1, padx=20)
 
 
